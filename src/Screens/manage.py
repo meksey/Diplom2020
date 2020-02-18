32ea7b11739e090da8b278581bafb34c8de74438
meksey <malyugin.lesha36@gmail.com>
2020-02-18 14:02:52 +0300

    Добавлена демонстрация показа рекоммендаций

--- src/Screens/manage.py
+++ src/Screens/manage.py
@@ -1,5 +1,33 @@
 from kivy.uix.screenmanager import Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivymd.uix.label import MDLabel
+from kivy.properties import StringProperty, ListProperty
 
 
 class ManageScreen(Screen):
-	pass
+	def on_enter(self, *args):
+
+		recs = {
+			'Рекоммендации по питанию': ['Есть меньше мучного', 'Пить больше кофе', 'Питаться энергией солнца'],
+			'Рекоммендации по образу жизни': ['Больше проводить времени на улице', 'Меньше курить', 'Не находиться на солнце слишком много'],
+		}
+
+		for key, value in recs.items():
+			self.ids.grid.add_widget(RecommendationCard(category=key, variants=value))
+			self.ids.grid.add_widget(RecommendationCard(category=key, variants=value))
+			self.ids.grid.add_widget(RecommendationCard(category=key, variants=value))
+
+
+
+
+class RecommendationCard(BoxLayout):
+	category = StringProperty()
+	variants = ListProperty()
+
+	def on_kv_post(self, base_widget):
+		for el in self.variants:
+			self.ids.rec_list.add_widget(RecommendationRow(variant=el))
+
+
+class RecommendationRow(BoxLayout):
+	variant = StringProperty()
